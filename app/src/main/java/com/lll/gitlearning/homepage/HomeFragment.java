36e5ca8fa8c494470613423d9f96dc2e66e97335
liulonglong <longlong.liu@pcitc.com>
2019-10-21 18:17:10 +0800

    创建首页模块

---
+++ app/src/main/java/com/lll/gitlearning/homepage/HomeFragment.java
@@ -0,0 +1,10 @@
+package com.lll.gitlearning.homepage;
+
+/**
+ * TODO:describe what the class or interface does.
+ *
+ * @author runningDigua
+ * @date 2019/10/21
+ */
+public class HomeFragment {
+}
---
+++ app/src/main/java/com/lll/gitlearning/homepage/activity/DetailActivity.java
@@ -0,0 +1,12 @@
+package com.lll.gitlearning.homepage.activity;
+
+import com.lll.gitlearning.base.BaseActivity;
+
+/**
+ * TODO:describe what the class or interface does.
+ *
+ * @author runningDigua
+ * @date 2019/10/21
+ */
+public class DetailActivity extends BaseActivity {
+}

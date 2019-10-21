5a3d1a6411f4d6b6a48f587e60f2464b0ee5f72f
liulonglong <longlong.liu@pcitc.com>
2019-10-21 18:22:12 +0800

    修改BaseActivity

--- app/src/main/java/com/lll/gitlearning/MainActivity.java
+++ app/src/main/java/com/lll/gitlearning/MainActivity.java
@@ -1,14 +1,21 @@
 package com.lll.gitlearning;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import com.lll.gitlearning.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
 
     }
 }

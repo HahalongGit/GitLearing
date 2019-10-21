91c6db1d2fcde544e4edb858650749ca914345e9
liulonglong <longlong.liu@pcitc.com>
2019-10-21 18:17:54 +0800

    创建首页模块

--- app/src/main/java/com/lll/gitlearning/homepage/HomeFragment.java
+++ app/src/main/java/com/lll/gitlearning/homepage/HomeFragment.java
@@ -1,10 +1,20 @@
 package com.lll.gitlearning.homepage;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.lll.gitlearning.base.BaseFragment;
+
 /**
- * TODO:describe what the class or interface does.
+ * 创建首页
  *
  * @author runningDigua
  * @date 2019/10/21
  */
-public class HomeFragment {
+public class HomeFragment extends BaseFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
--- app/src/main/java/com/lll/gitlearning/homepage/activity/DetailActivity.java
+++ app/src/main/java/com/lll/gitlearning/homepage/activity/DetailActivity.java
@@ -3,10 +3,13 @@ package com.lll.gitlearning.homepage.activity;
 import com.lll.gitlearning.base.BaseActivity;
 
 /**
- * TODO:describe what the class or interface does.
+ * 详情Activity
  *
  * @author runningDigua
  * @date 2019/10/21
  */
 public class DetailActivity extends BaseActivity {
+
+
+
 }

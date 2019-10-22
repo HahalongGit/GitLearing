76a72e9813a4e3e82e913dfc176c6eca0b4887d6
liulonglong <longlong.liu@pcitc.com>
2019-10-22 10:18:42 +0800

    修改baseFragment,创建MineFragment

--- app/src/main/java/com/lll/gitlearning/base/BaseFragment.java
+++ app/src/main/java/com/lll/gitlearning/base/BaseFragment.java
@@ -1,8 +1,13 @@
 package com.lll.gitlearning.base;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * BaseFragment
@@ -10,10 +15,36 @@ import android.support.v4.app.Fragment;
  * @author runningDigua
  * @date 2019/10/21
  */
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
+
+    private Context mContext;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(mContext).inflate(getLayoutId(), container, false);
+        initView();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        addListener();
+    }
+
+    protected abstract int getLayoutId();
+
+    protected abstract void initView();
+
+    protected void addListener() {
+
+    }
+
 }
---
+++ app/src/main/java/com/lll/gitlearning/mine/MineFragment.java
@@ -0,0 +1,23 @@
+package com.lll.gitlearning.mine;
+
+import com.lll.gitlearning.R;
+import com.lll.gitlearning.base.BaseFragment;
+
+/**
+ * 我的页面
+ *
+ * @author runningDigua
+ * @date 2019/10/22
+ */
+public class MineFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_mine_layout;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}

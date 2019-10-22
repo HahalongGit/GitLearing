20bc3ddad1bd31d58667e08486545d3afbad3b5f
liulonglong <longlong.liu@pcitc.com>
2019-10-22 18:31:42 +0800

    添加MinePresenter

--- app/src/main/java/com/lll/gitlearning/mine/impl/MinePresenter.java
+++ app/src/main/java/com/lll/gitlearning/mine/impl/MinePresenter.java
@@ -1,18 +1,10 @@
 package com.lll.gitlearning.mine.impl;
 
 /**
- * 创建MinePresenter
+ * TODO:describe what the class or interface does.
  *
  * @author runningDigua
  * @date 2019/10/22
  */
 public class MinePresenter {
-
-    private static final String TAG = "MinePresenter";
-
-    void setData() {
-
-    }
-
 }
-

a16b3ed43f45a9c97bb475b3ac07b8cd50f89213
liulonglong <longlong.liu@pcitc.com>
2019-10-24 19:39:56 +0800

    修改dev_1.0.2_v2提交

--- app/src/main/java/com/lll/gitlearning/base/BaseResponse.java
+++ app/src/main/java/com/lll/gitlearning/base/BaseResponse.java
@@ -1,10 +1,40 @@
 package com.lll.gitlearning.base;
 
 /**
- * TODO:describe what the class or interface does.
+ * 
  *
  * @author runningDigua
  * @date 2019/10/21
  */
-public class BaseResponse {
+public class BaseResponse<T> {
+
+    private int code;
+
+    private String message;
+
+    private T data;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
 }

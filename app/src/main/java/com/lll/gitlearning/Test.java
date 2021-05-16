8f5bba8c9d43fa51bb5e9841002d9f228d0111e1
刘龙龙 <liulonglong@zhihu.com>
2021-05-16 12:08:21 +0800

    测试合并分支

--- app/src/main/java/com/lll/gitlearning/Test.java
+++ app/src/main/java/com/lll/gitlearning/Test.java
@@ -1,13 +1,13 @@
 package com.lll.gitlearning;
 
 /**
- * TODO:describe what the class or interface does.
+ * 测试git命令
  *
  * @author zhihu
  * @date 16.5.21
  */
 class Test {
 
-    //测试添加到git 后再移除git管理git
+    //测试添加到git 后再移除git管理命令：git rm --cached 文件名称
 
 }

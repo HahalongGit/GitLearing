4ce9ced382f5e73e46988d95c6477e8dffe90dac
刘龙龙 <liulonglong@zhihu.com>
2021-05-16 11:51:48 +0800

    测试git命令 使用git rm --cached 文件名 移除git管理

--- app/src/main/java/com/lll/gitlearning/Test.java
+++ app/src/main/java/com/lll/gitlearning/Test.java
@@ -1,8 +1,13 @@
 package com.lll.gitlearning;
+
 /**
  * TODO:describe what the class or interface does.
  *
  * @author zhihu
  * @date 16.5.21
- */   class Test   {
+ */
+class Test {
+
+    //测试添加到git 后再移除git管理git
+
 }
